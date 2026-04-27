93eabc7eba61a4dd3f1d6bd2bffeef08e26d75b4
clarasoual <soual.clara@gmail.com>
2026-04-27 17:00:56 +0200

    balise base head

--- PROJET/UTILISATEUR/USR-index.php
+++ PROJET/UTILISATEUR/USR-index.php
@@ -22,6 +22,7 @@ $temoignages = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <base href="/PROJET/UTILISATEUR/">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
     <link rel="stylesheet" href="../CSS/style_global.css">
